fbb9576e3b54dac7e2d2f1b7d90abb9d59d04c5d
Melanie Myers <melanie.myers@365smartshop.com>
2017-01-20 15:24:22 -0500

    Order component

---
+++ catch-of-the-day/src/components/Inventory.js
@@ -0,0 +1,12 @@
+/**
+ * Created by melanie.myers on 1/20/17.
+ */
+import React from 'react';
+
+class Inventory extends React.component{
+    render(){
+        return()
+    }
+}
+
+export default React;
---
+++ catch-of-the-day/src/components/Order.js
@@ -0,0 +1,13 @@
+/**
+ * Created by melanie.myers on 1/20/17.
+ */
+
+import React from 'react';
+
+class Order extends React.component{
+    render(){
+        return()
+    }
+}
+
+export default Order;
