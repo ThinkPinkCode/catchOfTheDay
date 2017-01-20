eaee6303b8c9945ac9bb46b56d03933b88ae1e8b
Melanie Myers <melanie.myers@365smartshop.com>
2017-01-20 14:46:52 -0500

    initial commit

---
+++ catch-of-the-day/src/components/App.js
@@ -0,0 +1,19 @@
+/**
+ * Created by melanie.myers on 1/17/17.
+ */
+import React from 'react';
+import Header from './Header';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="catch-of-the-day">
+                <div className="menu">
+                    <Header />
+                </div>
+
+            </div>
+        )
+    }
+}
+export default App;
---
+++ catch-of-the-day/src/components/Header.js
@@ -0,0 +1,14 @@
+/**
+ * Created by melanie.myers on 1/17/17.
+ */
+
+import React from 'react';
+
+class Header extends React.Component {
+    render() {
+        return (
+            <p>Header</p>
+        )
+    }
+}
+export default Header;
