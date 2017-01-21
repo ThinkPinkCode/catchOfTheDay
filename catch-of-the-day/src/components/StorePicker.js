43c21b503c750c0824d4644674c47a38bc03fcdf
Melanie Myers <melanie.myers@365smartshop.com>
2017-01-21 18:33:58 -0500

    added router transition

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,17 +2,37 @@
  * Created by melanie.myers on 1/17/17.
  */
 import React from 'react';
-import { getFunName } from '../helpers';
+import {getFunName} from '../helpers';
 
-class StorePicker extends React.Component{
-    render(){
+class StorePicker extends React.Component {
+    goToStore(event) {
+        event.preventDefault();
+        console.log('You changed the URL');
+        //first grab the text from the box
+        const storeId = this.storeInput.value;
+        console.log(storeId);
+        //second we're going to transition from / to /store/:storeId
+        this.context.router.transitionTo(`/store/${storeId}`)
+    }
+
+    render() {
         return (
-            <form className="store-selector">
+            <form className="store-selector" onSubmit={this.goToStore.bind(this)}>
                 <h2>Please Enter A Store</h2>
-                <input type="text" required placeholder="Store Name" defaultValue = {getFunName()}/>
-                <button type="submit">Visit Store → </button>
+                <input type="text" required placeholder="Store Name"
+                       defaultValue={getFunName()}
+                       ref={(input) => {
+                           this.storeInput = input
+                       }}/>
+                <button type="submit">Visit Store →</button>
             </form>
         )
     }
+
 }
+    StorePicker.contextTypes = {
+        router: React.PropTypes.object
+
+}
+
 export default StorePicker;
