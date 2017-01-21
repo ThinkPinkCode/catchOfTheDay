764870209dbef60418406a38dea1aa5fe5fa07dc
Melanie Myers <melanie.myers@365smartshop.com>
2017-01-20 21:59:53 -0500

    added NotFound component

---
+++ catch-of-the-day/src/components/NotFound.js
@@ -0,0 +1,16 @@
+/**
+ * Created by melanie.myers on 1/20/17.
+ */
+/**
+ * Created by melanie.myers on 1/20/17.
+ */
+
+import React from 'react';
+
+class NotFound extends React.Component {
+    render() {
+        return (<h2>NotFound</h2>)
+    }
+}
+
+export default NotFound;
